a24462cc1234e09d09a4b1ac69acb7a5c5c6a88c
Gustavo Maciel Pires <gustavomp.ti@gmail.com>
2016-10-18 20:01:12 -0200

    Update 1001.py

--- 1001.py
+++ 1001.py
@@ -1,12 +1,3 @@
-import java.io.IOException;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) throws IOException {
-        int a,b,resultado;
-	    Scanner teclado = new Scanner(System.in);
-    	a = teclado.nextInt();
-    	b = teclado.nextInt();
-    	resultado = a + b;
-    	System.out.println("X = " + resultado);
-    }
-}
+n1 = int(input())
+n2 = int(input())
+print(n1+n2)
